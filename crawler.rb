f3b4b3bd916f9a2ffbe7e6802b9b3348870f80bf
Yukai Huang <yukaihuang1993@hotmail.com>
2015-05-08 20:56:27 +0800

    fix 1031 file encoding

--- crawler.rb
+++ crawler.rb
@@ -3,10 +3,12 @@ require 'pry'
 require 'json'
 
 
-courses = []  
+courses = []
 Dir.glob('1031/*.html').each do |filename|
-	string = File.read(filename)
-	document = Nokogiri::HTML(string.encode("utf-8",:invalid => :replace, :undef => :replace))
+	file = File.open(filename, 'r:utf-8')
+  string = file.read
+	document = Nokogiri::HTML(string.encode!("utf-8", :invalid => :replace, :undef => :replace, :replace => '?'))
+  # .encode("utf-8",:invalid => :replace, :undef => :replace)
 
 	document.css('table tr:not(:first-child)').each do |row|
     datas = row.css('td')
@@ -15,19 +17,16 @@ Dir.glob('1031/*.html').each do |filename|
       grade: datas[0] && datas[0].text,
       serial: datas[1] && datas[1].text,
       class_type: datas[2] && datas[2].text,
-      name: datas[3] && datas[3].text,
-      lecturer: datas[4] && datas[4].text,
-      credits: datas[6] && datas[6].text,
+      name: datas[3] && datas[3].text && datas[3].text.strip,
+      lecturer: datas[4] && datas[4].text && datas[4].text.strip,
+      credits: datas[6] && datas[6].text && datas[6].text.to_i,
       required_or_elective: datas[7] && datas[7].text,
       time_location: datas[8] && datas[8].text,
       type: datas[10] && datas[10].text,
       outline: datas[11] && datas[11].css('a')[0]  && datas[11].css('a')[0][:href],
       note: datas[12] && datas[12].text
     }
-
-
-
 	end
-end	
-    
+end
+
 File.open('courses.json','w'){|file| file.write(JSON.pretty_generate(courses))}
